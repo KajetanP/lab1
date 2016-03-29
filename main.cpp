d5b8f7f44a4a6a5dd3cf75121f50b0d2984c73f4
student <225940@student.pwr.edu.pl>
2016-03-29 19:21:35 +0200

    bugged ver.

--- main.cpp
+++ main.cpp
@@ -7,17 +7,41 @@ void drawCharSquare(char **square, int n);
 
 int main(int argc, char **argv)
 {
-	int n;
+	int n = atoi(argv[1]);
+	srand(atoi(argv[2]));
 	drawCharSquare(charSquare(n), n);	
 }
 
 char **charSquare(int n)
 {
-	char **square;
+	// CREATION
+	char **square = new char*[n];
+	for(int i = 0 ; i < n ; i++)
+	{
+		square[i] = new char[n];
+	}
+
+	// FILLING
+	for(int i = 0 ; i < n * n ; i++)
+	{
+		*((char*)square + i) = rand() % 26 + 97;
+	}
+
 	return square;
 }
 
 void drawCharSquare(char **square, int n)
 {
+	// DRAWING
+	for(int i = 0 ; i < n * n ; i++)
+	{
+		((i + 1) % n) ? printf("%c", *((char*)square + i)) : printf("%c \n", *((char*)square + i));
+	}
 
+	// DESTRUCTION
+	for(int i = 0 ; i < n ; i++)
+	{
+		delete [] square[i];
+	}
+	delete [] square;
 }
